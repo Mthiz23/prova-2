1cb02a0f8309f344cb37cbeb5a83495f808ffb2c
MATHEUS MOREIRA DE SOUZA <matheus_souza18@estudante.sesisenai.org.br>
2025-04-24 09:49:45 -0300

    métodos dos bagulho

--- prova.js
+++ prova.js
@@ -8,29 +8,35 @@ class personagem {
         this.energia = tipo;
     }
 }
-let p1 = new personagem (100, 50, 50, normal)
+let p1 = new personagem (100, 50, 50, comum)
+let p2 = new personagem (100, 50, 50, guerreiro)
 
 class guerreiro extends personagem {
     constructor(vida, forca, energia, tipo){
-        super(vida);
-        super(forca);
-        super(energia);
-        super(tipo);
+        super(vida, forca, energia, tipo);
+        
+    }
+    atacar(algo){
+        let ataque = 20;
+        console.log("guerreiro atacou");
+        algo.vida -= ataque;
+        if (algo.vida <=0) {
+            console.log("`${this.tipo}´, morreu ");
+        } else if(algo.vida > 0) {
+            console.log("proximo turno")
+        }
     }
 }
 class mago extends personagem {
     constructor(vida, forca, energia, tipo){
-        super(vida);
-        super(forca);
-        super(energia);
-        super(tipo);
+        super(vida, forca, energia, tipo);
+        
     }
+    
 }
 class arqueiro extends personagem {
     constructor(vida, forca, energia, tipo){
-        super(vida);
-        super(forca);
-        super(energia);
-        super(tipo);
+        super(vida, forca, energia, tipo);
+        
     }
 }
