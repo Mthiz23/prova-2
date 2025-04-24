b563c890ab2e6bc8cf038308e0055d82e1a34228
MATHEUS MOREIRA DE SOUZA <matheus_souza18@estudante.sesisenai.org.br>
2025-04-24 10:42:06 -0300

    Aplicação de polimorfismo no método atacar + método específico do guerreiro

--- prova.js
+++ prova.js
@@ -15,9 +15,14 @@ class guerreiro extends personagem {
     constructor(vida, forca, energia, tipo){
         super(vida, forca, energia, tipo);
         
+    } defenderescudo (recuperou){
+        let defender = 10;
+        console.log ("guerreiro se defendeu com o escudo")
+        recuperou.vida += defender;
+        console.log (`guerreiro recuperou ${this.vida}`)
     }
     atacar(algo){
-        let ataque = 30;
+        let ataque = 25;
         console.log("guerreiro atacou");
         algo.vida -= ataque;
         if (algo.vida <=0) {
@@ -37,16 +42,34 @@ class mago extends personagem {
         console.log ("mago se regenerou")
         cura.vida += regenerar;
         console.log (`mago possui ${this.vida}`)
+    } atacar(algo){
+        let ataque = 10;
+        console.log("mago atacou");
+        algo.vida -= ataque;
+        if (algo.vida <=0) {
+            console.log("`${this.tipo}´, morreu ");
+        } else if(algo.vida > 0) {
+            console.log("proximo turno")
+        }
     }
 }
 class arqueiro extends personagem {
     constructor(vida, forca, energia, tipo){
         super(vida, forca, energia, tipo);
         
-    } defender (recuperou){
+    } defenderarco (recuperou){
         let defender = 10;
-        console.log ("arqueiro se defendeu")
+        console.log ("arqueiro se defendeu com o arco")
         recuperou.vida += defender;
         console.log (`arqueiro recuperou ${this.vida}`)
+    } atacar(algo){
+        let ataque = 20;
+        console.log("arqueiro atacou");
+        algo.vida -= ataque;
+        if (algo.vida <=0) {
+            console.log("`${this.tipo}´, morreu ");
+        } else if(algo.vida > 0) {
+            console.log("proximo turno")
+        }
     }
 }
